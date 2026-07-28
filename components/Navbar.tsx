418cb62c7965fc5b8fd3e5501ee2bd9656dfe356
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-07-28 12:06:02 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,8 @@ export default function Navbar() {
           className="w-full bg-gray-900 text-sm text-gray-200 rounded-full px-4 py-2 pl-10 border border-gray-700 focus:outline-none focus:border-purple-500 transition"
         />
         <svg className="w-4 h-4 absolute left-3 top-2.5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+          {/* HURUF 'M' DITAMBAHKAN DI AWAL 'd' DI BAWAH INI */}
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
         </svg>
       </form>
     </nav>
